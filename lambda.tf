4067905c1d2878d87710c380216967c4494473d1
AkilBlanchard <akilblanchard@github.com>
2023-10-19 11:04:36 -0500

    updated policy and role

--- lambda.tf
+++ lambda.tf
@@ -1,71 +1,66 @@
-#IAM Role for Lambda
-resource "aws_iam_role" "crud_role" {
-  name = "crud-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-#IAM Policy for Lambda Function
-resource "aws_iam_policy" "crud_policy" {
-  name = "crud-policy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ]
-      Resource = ["*"]
-      }, {
-      Effect = "Allow"
-      Action = [
-        "dynamodb:DeleteItem",
-        "dynamodb:GetItem",
-        "dynamodb:PutItem",
-        "dynamodb:Query",
-        "dynamodb:Scan",
-        "dynamodb:UpdateItem"
-      ]
-      Resource = ["*"]
-    }]
-  })
-}
-
-
-#Attach IAM Role
-resource "aws_iam_role_policy_attachment" "example" {
-  policy_arn = aws_iam_policy.crud_policy.arn
-  role       = aws_iam_role.crud_role.name
-}
 
 
 #Lambda Data File
 data "archive_file" "lambda_archive" {
-  source_file = "index.mjs"
+  source_file = "index.js"
   output_path = "lambdas/index.zip"
   type        = "zip"
 }
 
-#Lambda Function
-resource "aws_lambda_function" "http_crud_func" {
-  function_name = "HttpCrudLambda"
-  filename      = "lambdas/index.zip"
- source_code_hash = data.archive_file.lambda_archive.output_base64sha256
-  handler = "index.handler"
-  role    = aws_iam_role.crud_role.arn
-  runtime = "nodejs14.x"
+resource "aws_lambda_function" "crud_lambda" {
+  function_name = "crud-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  filename      = "lambdas/index.zip" # Replace with the path to your Lambda deployment package (ZIP file)
+
+  source_code_hash = data.archive_file.lambda_archive.output_base64sha256
 }
 
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_execution_policy"
+  description = "IAM policy for Lambda execution role"
 
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = "dynamodb:DeleteItem",
+        Effect   = "Allow",
+        Resource = "arn:aws:dynamodb:us-west-1:123456789012:table/http-crud-tutorial-items"
+      },
+      {
+        Action   = "dynamodb:GetItem",
+        Effect   = "Allow",
+        Resource = "arn:aws:dynamodb:us-west-1:123456789012:table/http-crud-tutorial-items"
+      },
+      {
+        Action   = "dynamodb:PutItem",
+        Effect   = "Allow",
+        Resource = "arn:aws:dynamodb:us-west-1:123456789012:table/http-crud-tutorial-items"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  policy_arn = aws_iam_policy.lambda_policy.arn
+  role       = aws_iam_role.lambda_role.name
+  }
