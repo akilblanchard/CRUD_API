21880d8bd249fc8b0d9b3965688b24686130b5b8
AkilBlanchard <akilblanchard@github.com>
2023-10-19 10:30:22 -0500

    updated outputs values

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,9 @@
 output "function_name" {
   description = "Name of the Lambda function."
-
-  value = aws_lambda_function.http_crud_func.function_name
+  value       = aws_lambda_function.crud-lambda.function_name
 }
-
-
-
 
 output "base_url" {
   description = "Base URL for API Gateway stage."
-
-  value =  aws_apigatewayv2_stage.crud_api.invoke_url
+  value       = aws_apigatewayv2_stage.stage_crud_api.invoke_url
 }
