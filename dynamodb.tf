b9a3348c14b03e72d00efa2d57820cab1510bed8
AkilBlanchard <akilblanchard@github.com>
2023-11-09 22:03:54 -0600

    Added read and write capacity to the table

--- dynamodb.tf
+++ dynamodb.tf
@@ -2,6 +2,8 @@
 resource "aws_dynamodb_table" "crud-api-db" {
   name           = "crud-api-db"
   billing_mode   = "PROVISIONED"
+  read_capacity  = 1  
+  write_capacity = 1
   hash_key       = "id"
 
   attribute {
@@ -21,29 +23,5 @@ resource "aws_dynamodb_table" "crud-api-db" {
 
   tags = {
     Name = "crud-api-db"
-  }
-}
-
-#DynamoDB AutoScaling
-resource "aws_appautoscaling_target" "dynamodb_table_read_target" {
-  max_capacity       = 100
-  min_capacity       = 5
-  resource_id        = "table/crud-api-db"
-  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
-  service_namespace  = "dynamodb"
-}
-resource "aws_appautoscaling_policy" "dynamodb_table_read_policy" {
-  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_read_target.resource_id}"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.dynamodb_table_read_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.dynamodb_table_read_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.dynamodb_table_read_target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "DynamoDBReadCapacityUtilization"
-    }
-
-    target_value = 70 #set utilization to 70%
   }
 }
